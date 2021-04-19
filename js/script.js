72c943f9833ad82b4d93e743612192c0fa7d2553
Mirko <mirkosbaglia2001@gmail.com>
2021-04-19 16:04:58 +0200

    Aggiunte bandiere e serie tv

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ function initVue() {
         el: '#app',
         data: {
           movies:[],
-          searchMovie:''
+          searchInput:'',
+
+          series:[],
+
         },
 
         methods:{
@@ -14,21 +17,51 @@ function initVue() {
 
              params: {
                'api_key': '240c7ea35ff41488d7c3cc3a869f8ccc',
-               'query':this.searchMovie
+               'query':this.searchInput
 
              }
            })
 
            .then(data => {
-             console.log(data.data.results);
+             //console.log(data.data.results);
              //console.log(data.data.results[0].original_title);
 
              const results = data.data.results;
              this.movies = results;
              //console.log(this.movies);
+             
            })
            .catch(() => console.log('error'));
-          }
+         },
+
+
+
+        //serie tv
+         sendSeries: function(){
+          //console.log(this.searchseries);
+
+          axios.get('https://api.themoviedb.org/3/search/tv' ,{
+
+            params: {
+              'api_key': '240c7ea35ff41488d7c3cc3a869f8ccc',
+              'query':this.searchInput
+
+
+            }
+          })
+
+          .then(data => {
+            console.log(data.data.results);
+            //console.log(data.data.results[0].original_title);
+
+            const resultseries = data.data.results;
+            this.series = resultseries;
+            console.log(this.series);
+
+          })
+          .catch(() => console.log('error'));
+         }
+
         }
 
 
